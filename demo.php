d8d4dd9f4c2739525b5d47b8cd7f94d840d5ea83
chandraveer <chandraveersingh.90@gmail.com>
2017-02-02 12:33:16 +0530

    add curl request for testing

--- demo.php
+++ demo.php
@@ -1,7 +1,18 @@
 <?php
-   $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+   /*$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
    echo '<pre>';
    print_r($url);
-   echo '</pre>';
+   echo '</pre>';*/
+   	$process = curl_init("https://store-accxx7oobc.mybigcommerce.com/api/v2/time");
+	curl_setopt($process, CURLOPT_HTTPHEADER, array('Content-Type: application/xml'));
+	curl_setopt($process, CURLOPT_HEADER, 1);
+	curl_setopt($process, CURLOPT_USERPWD, "cvssolutions" . ":" . "838d58977a565ba203cb77a829bd72fd48b70fd9");
+	curl_setopt($process, CURLOPT_TIMEOUT, 30);
+	curl_setopt($process, CURLOPT_POST, 1);
+	curl_setopt($process, CURLOPT_POSTFIELDS, $payloadName);
+	curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+	$return = curl_exec($process);
+	curl_close($process);
+	echo $return;
 ?>
 
