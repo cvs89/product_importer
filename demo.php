3c9540eb300ccd394379627a307728c6c5ac1e81
chandraveer <chandraveersingh.90@gmail.com>
2017-02-02 12:44:30 +0530

    add curl request for testing

--- demo.php
+++ demo.php
@@ -3,15 +3,30 @@
    echo '<pre>';
    print_r($url);
    echo '</pre>';*/
-   	$process = curl_init("https://store-accxx7oobc.mybigcommerce.com/api/v2/time");
-	//curl_setopt($process, CURLOPT_HTTPHEADER, array('Content-Type: application/xml'));
-	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-	curl_setopt($process, CURLOPT_USERPWD, "cvssolutions" . ":" . "838d58977a565ba203cb77a829bd72fd48b70fd9");
-	curl_setopt($process, CURLOPT_TIMEOUT, 30);
-	curl_setopt($process, CURLOPT_POST, 1);
-	curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
-	$return = curl_exec($process);
-	curl_close($process);
-	echo $return;
+
+
+$submit_url = "https://store-accxx7oobc.mybigcommerce.com/api/v2/time";
+
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC ) ;
+curl_setopt($curl, CURLOPT_USERPWD, "cvssolutions:838d58977a565ba203cb77a829bd72fd48b70fd9");
+curl_setopt($curl, CURLOPT_SSLVERSION,3);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($curl, CURLOPT_HEADER, true);
+//curl_setopt($curl, CURLOPT_POST, true);
+//curl_setopt($curl, CURLOPT_POSTFIELDS, $params );
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
+curl_setopt($curl, CURLOPT_URL, $submit_url);
+
+$data = split("text/html", curl_exec($curl) );
+$temp = split("\r\n", $data[1]) ;
+
+$result = unserialize( $temp[2] ) ;
+
+print_r($result);
+curl_close($curl); 
 ?>
 
