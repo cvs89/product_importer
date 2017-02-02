b4a89e3062ee6e0a91456b3b7f6d69efb8d0f2de
chandraveer <chandraveersingh.90@gmail.com>
2017-02-02 13:00:24 +0530

    add curl request for testing

--- demo.php
+++ demo.php
@@ -17,11 +17,11 @@ curl_setopt($curl, CURLOPT_HEADER, true);
 //curl_setopt($curl, CURLOPT_POST, true);
 //curl_setopt($curl, CURLOPT_POSTFIELDS, $params );
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
+//curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
 curl_setopt($curl, CURLOPT_URL, $submit_url);
 
 $return = curl_exec($curl);
-print_r($return);
+echo $return;
 curl_close($curl); 
 ?>
 
