a4c6bcebf3a277ec0cf55c683c79ff09495ec06d
chandraveer <chandraveersingh.90@gmail.com>
2017-02-02 12:38:01 +0530

    add curl request for testing

--- demo.php
+++ demo.php
@@ -5,11 +5,10 @@
    echo '</pre>';*/
    	$process = curl_init("https://store-accxx7oobc.mybigcommerce.com/api/v2/time");
 	//curl_setopt($process, CURLOPT_HTTPHEADER, array('Content-Type: application/xml'));
-	curl_setopt($process, CURLOPT_HEADER, 1);
+	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 	curl_setopt($process, CURLOPT_USERPWD, "cvssolutions" . ":" . "838d58977a565ba203cb77a829bd72fd48b70fd9");
 	curl_setopt($process, CURLOPT_TIMEOUT, 30);
 	curl_setopt($process, CURLOPT_POST, 1);
-	curl_setopt($process, CURLOPT_POSTFIELDS, $payloadName);
 	curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
 	$return = curl_exec($process);
 	curl_close($process);
