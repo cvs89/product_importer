ec084d976deda61cb6f5ca4d3dbea5d6a781d28f
chandraveer <chandraveersingh.90@gmail.com>
2017-02-02 13:50:16 +0530

    add bigcommerce library

--- demo.php
+++ demo.php
@@ -1,6 +1,6 @@
 <?php
 //header('Content-Type: application/json');
-$submit_url = "https://store-accxx7oobc.mybigcommerce.com/api/v2/products.json";
+/*$submit_url = "https://store-accxx7oobc.mybigcommerce.com/api/v2/products.json";
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC ) ;
 curl_setopt($curl, CURLOPT_USERPWD, "cvssolutions:838d58977a565ba203cb77a829bd72fd48b70fd9");
@@ -11,6 +11,18 @@ $return = curl_exec($curl);
 print_r(json_decode($return));
 echo json_last_error();
 curl_close($curl);
-exit();
+exit();*/
+use Bigcommerce\Api\Client as Bigcommerce;
+
+Bigcommerce::configure(array(
+    'store_url' => 'https://store-accxx7oobc.mybigcommerce.com',
+    'username'  => 'cvssolutions',
+    'api_key'   => '838d58977a565ba203cb77a829bd72fd48b70fd9'
+));
+
+$ping = Bigcommerce::getTime();
+
+if ($ping) 
+  echo $ping->format('H:i:s');
 ?>
 
