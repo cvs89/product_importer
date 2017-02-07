f02b0f932627fe28c20e27acba7157b3c7168107
chandraveer <chandraveersingh.90@gmail.com>
2017-02-07 18:22:10 +0530

    add and test load and auth url

--- product.php
+++ product.php
@@ -24,5 +24,31 @@
 		print_r($row);
 	}
 	echo '</pre>';
+	if(true){
+	$data = array(
+	    "client_id" => "gmeaga68mcb9zv8gz6an6vq3zjtakic",
+	    "client_secret" => "r6j81c49tnqwwelhimnlcxivybrg2s8",
+	    "redirect_uri" => "https://importproduct.herokuapp.com/auth.php",
+	    "grant_type" => "authorization_code",
+	    "code" => $_GET["code"],
+	    "scope" => $_GET["scope"],
+	    "context" => $_GET["context"],
+	);
+	$postfields = http_build_query($data);
+
+	$ch = curl_init();                    
+	$url = "https://api.bigcommerce.com/".$rows['context']."/v2/products";
+	curl_setopt($ch, CURLOPT_URL,$url);
+	//curl_setopt($ch, CURLOPT_POST, true);
+	//curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$output = curl_exec ($ch);
+	curl_close ($ch);
+
+	$obj = json_decode($output);
+	echo '<pre>';
+	print_r($obj);
+	echo '</pre>';
+}
 ?>
 
