f31f3e408af779e7a3836430c22d9bffe64fa757
chandraveer <chandraveersingh.90@gmail.com>
2017-02-07 17:28:54 +0530

    add and test load and auth url

--- load.php
+++ load.php
@@ -30,6 +30,11 @@ if(!(is_array($data) && count($data)>0)){
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $database = ltrim($url['path'],'/');
 $link = mysqli_connect($url['host'], $url['user'], $url['pass'], $database);
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
 $result = mysqli_query($link,"SELECT * FROM products");
 echo '<pre>'
 while($row = mysqli_fetch_assoc($result)){
