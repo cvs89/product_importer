8cf16064fb62ca8e0a0c461ecda468f8379a5db2
chandraveer <chandraveersingh.90@gmail.com>
2017-02-07 18:00:14 +0530

    add and test load and auth url

--- load.php
+++ load.php
@@ -27,20 +27,7 @@ function verifysignedrequest($signedRequest)
 if(!(is_array($data) && count($data)>0)){
 	die("Bad signed request from BigCommerce!");
 }
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$database = ltrim($url['path'],'/');
-$link = mysqli_connect($url['host'], $url['user'], $url['pass'], $database);
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-$result = mysqli_query($link,"SELECT * FROM products as a join variants as b on (a.id = b.product_id) limit 1");
-echo '<pre>';
-while($row = mysqli_fetch_assoc($result)){
-	print_r($row);
-}
-echo '</pre>';
+
 ?>
 <h1>Welcome to Product Importer</h1>
 <a href="product.php">Load Products</a>
--- product.php
+++ product.php
@@ -1,7 +1,17 @@
 <?php
-   $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-   echo '<pre>';
-   print_r($url);
-   echo '</pre>';
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$database = ltrim($url['path'],'/');
+	$link = mysqli_connect($url['host'], $url['user'], $url['pass'], $database);
+	/* check connection */
+	if (mysqli_connect_errno()) {
+	    printf("Connect failed: %s\n", mysqli_connect_error());
+	    exit();
+	}
+	$result = mysqli_query($link,"SELECT * FROM products as a join variants as b on (a.id = b.product_id) limit 1");
+	echo '<pre>';
+	while($row = mysqli_fetch_assoc($result)){
+		print_r($row);
+	}
+	echo '</pre>';
 ?>
 
