884561e3b8668d8d8ebd5493f22449bba9ea9936
chandraveer <chandraveersingh.90@gmail.com>
2017-02-07 15:25:22 +0530

    add and test load and auth url

--- load.php
+++ load.php
@@ -1,8 +1,11 @@
 <?php
    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 echo 'load';
+$data = explode('.',$_GET['signed_payload']);
    echo '<pre>';
-   print_r($_GET);
+   print_r($data);
    echo '</pre>';
+
+
 ?>
 
