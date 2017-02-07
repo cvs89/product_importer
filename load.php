247ee8a05f7ded477a616fa3b7fcd81e1c09bb86
chandraveer <chandraveersingh.90@gmail.com>
2017-02-07 17:34:01 +0530

    add and test load and auth url

--- load.php
+++ load.php
@@ -28,14 +28,14 @@ if(!(is_array($data) && count($data)>0)){
 	die("Bad signed request from BigCommerce!");
 }
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-echo $database = ltrim($url['path'],'/');
+$database = ltrim($url['path'],'/');
 $link = mysqli_connect($url['host'], $url['user'], $url['pass'], $database);
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-$result = mysqli_query($link,"SELECT * FROM products");
+$result = mysqli_query($link,"SELECT * FROM variants");
 echo '<pre>';
 while($row = mysqli_fetch_assoc($result)){
 	print_r($row);
