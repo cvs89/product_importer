a45f88bb1a38f7872f3b66a04b53f74f5b1c654e
chandraveer <chandraveersingh.90@gmail.com>
2017-02-07 12:08:59 +0530

    add and test load and auth url

--- load.php
+++ load.php
@@ -1,5 +1,6 @@
 <?php
    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+echo 'load';
    echo '<pre>';
    print_r($_GET);
    echo '</pre>';
--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,6 @@
 <?php
    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+   echo 'uninstall.php';
    echo '<pre>';
    print_r($_GET);
    echo '</pre>';
