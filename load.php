ec5a26771602625c50230e09ca80a563811c13ec
chandraveer <chandraveersingh.90@gmail.com>
2017-02-07 15:43:22 +0530

    add and test load and auth url

--- load.php
+++ load.php
@@ -1,7 +1,24 @@
 <?php
+function verifySignedRequest($signedRequest)
+{
+    list($encodedData, $encodedSignature) = explode('.', $signedRequest, 2); 
+
+    // decode the data
+    $signature = base64_decode($encodedSignature);
+        $jsonStr = base64_decode($encodedData);
+    $data = json_decode($jsonStr, true);
+
+    // confirm the signature
+    $expectedSignature = hash_hmac('sha256', $jsonStr, $clientSecret(), $raw = false);
+    if (!hash_equals($expectedSignature, $signature)) {
+        error_log('Bad signed request from BigCommerce!');
+        return null;
+    }
+    return $data;
+}
    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 echo 'load';
-$data = explode('.',$_GET['signed_payload']);
+$data = verifySignedRequest($_GET['signed_payload']);
    echo '<pre>';
    print_r($data);
    echo '</pre>';
