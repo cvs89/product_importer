15668e156ee038f60ed72f7c178d190f518725b9
chandraveer <chandraveersingh.90@gmail.com>
2017-02-08 17:29:29 +0530

    chack the phate

--- demo.php
+++ demo.php
@@ -1,43 +1,39 @@
 <?php
 //header('Content-Type: application/json');
 /*$submit_url = "https://store-accxx7oobc.mybigcommerce.com/api/v2/products.json";
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC ) ;
-curl_setopt($curl, CURLOPT_USERPWD, "cvssolutions:838d58977a565ba203cb77a829bd72fd48b70fd9");
-curl_setopt($curl, CURLOPT_HEADER, true);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_URL, $submit_url);
-$return = curl_exec($curl);
-print_r(json_decode($return));
-echo json_last_error();
-curl_close($curl);
-exit();*/
+ $curl = curl_init();
+ curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC ) ;
+ curl_setopt($curl, CURLOPT_USERPWD, "cvssolutions:838d58977a565ba203cb77a829bd72fd48b70fd9");
+ curl_setopt($curl, CURLOPT_HEADER, true);
+ curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($curl, CURLOPT_URL, $submit_url);
+ $return = curl_exec($curl);
+ print_r(json_decode($return));
+ echo json_last_error();
+ curl_close($curl);
+ exit();*/
 include_once 'config.php';
 include 'vendor/autoload.php';
 
-
 use Bigcommerce\Api\Client as Bigcommerce;
 use Bigcommerce\Api\Connection;
-if(isset($_GET['user'])){
-$select = "select * from stores where user_id='".$_GET['user']."'";
-$result_token = mysqli_query($link,$select);
-$rows = mysqli_fetch_assoc($result_token);
-Bigcommerce::configure(array(
-    'client_id' => 'gmeaga68mcb9zv8gz6an6vq3zjtakic',
-    'auth_token' => $rows['access_token'],
-    'store_hash' => 'accxx7oobc'
-));
-$ping = Bigcommerce::getTime();
-echo 'his';
-$products = Bigcommerce::getProducts();
-echo '<pre>';
-print_r($products );
-echo '</pre>';
-foreach ($products as $product) {
-    //echo $product->name;
-    //echo $product->price;
-}
-}else{
+if (isset($_GET['user'])) {
+	$select = "select * from stores where user_id='" . $_GET['user'] . "'";
+	$result_token = mysqli_query($link, $select);
+	$rows = mysqli_fetch_assoc($result_token);
+	Bigcommerce::configure(array('client_id' => 'gmeaga68mcb9zv8gz6an6vq3zjtakic', 'auth_token' => $rows['access_token'], 'store_hash' => 'accxx7oobc'));
+	$ping = Bigcommerce::getTime();
+	echo 'his';
+	//$products = Bigcommerce::getProducts();
+	$result = mysqli_query($link, "SELECT * FROM products as a join variants as b on (a.id = b.product_id)");
+	echo '<pre>';
+	while ($row = mysqli_fetch_assoc($result)) {
+		print_r($row);
+	}
+	//print_r($products );
+	echo '</pre>';
+
+} else {
 	echo 'Invalid request';
 }
 ?>
--- product.php
+++ product.php
@@ -2,60 +2,52 @@
 include_once 'config.php';
 include 'vendor/autoload.php';
 
-
 use Bigcommerce\Api\Client as Bigcommerce;
 use Bigcommerce\Api\Connection;
 
-	$user = '';
-	if(isset($_GET['user'])){
-		$user = $_GET['user'];
-	}else{
-		exit("No store Available");
-	}
-	$result_token = mysqli_query($link,"select * from stores where user_id=".$user);
-	$rows = mysqli_fetch_assoc($result_token);
-	echo $rows['access_token'];
-	echo '<br>';
-	Bigcommerce::configure(array(
-	    'client_id' => 'gmeaga68mcb9zv8gz6an6vq3zjtakic',
-	    'auth_token' => $rows['access_token'],
-	    'store_hash' => 'accxx7oobc'
-	));
-	
-	$result = mysqli_query($link,"SELECT * FROM products as a join variants as b on (a.id = b.product_id) limit 1, 1");
-	$filter = array("is_featured" => true);
+$user = '';
+if (isset($_GET['user'])) {
+	$user = $_GET['user'];
+} else {
+	exit("No store Available");
+}
+$result_token = mysqli_query($link, "select * from stores where user_id=" . $user);
+$rows = mysqli_fetch_assoc($result_token);
+echo $rows['access_token'];
+echo '<br>';
+Bigcommerce::configure(array('client_id' => 'gmeaga68mcb9zv8gz6an6vq3zjtakic', 'auth_token' => $rows['access_token'], 'store_hash' => 'accxx7oobc'));
+
+$result = mysqli_query($link, "SELECT * FROM products as a join variants as b on (a.id = b.product_id) limit 1, 1");
+$filter = array("is_featured" => true);
 
 $categories = Bigcommerce::getCategories();
-	echo '<pre>';
-	//print_r($categories);
-	foreach ($categories as $categorie) {
-		echo $categorie->id;
-	}
-	while($row = mysqli_fetch_assoc($result)){
-		print_r($row);
-		
-		$fields = array(
-					  "name" => $row['title'],
-					  "price" => $row['price'],
-					  "categories" => array(18),
-					  "weight" => $row['grams'],
-					  "sku" => $row['sku'],
-					  'type' => 'physical',
-					  'availability' => 'available',
-					  'is_visible' => true
-					);
-			//print_r($fields);
-			//$product = new stdClass();
-			//$product = (object) $fields;
-			try{
-				$products =	Bigcommerce::createProduct($fields);
-			    print_r($products);
-			    echo 'try';
-			}catch(Bigcommerce\Api\Error $error){
-				echo $error->getCode();
-      			echo $error->getMessage();
-			}
-	}
-	echo '</pre>';
+echo '<pre>';
+//print_r($categories);
+foreach ($categories as $categorie) {
+	echo $categorie -> id;
+}
+while ($row = mysqli_fetch_assoc($result)) {
+	print_r($row);
 
+	$fields = array("name" => $row['title'], "price" => $row['price'], "categories" => array(18), "weight" => $row['grams'], "sku" => $row['sku'], 'type' => 'physical', 'availability' => 'available', 'is_visible' => true);
+	if (isset($row['body']) && $row['body'] != '') {
+		$fields['description'] = $row['body'];
+	}
+	if (isset($row['handle']) && $row['handle'] != '') {
+		$fields['custom_url'] = $row['handle'];
+	}
+	//if(custom_url)
+	//print_r($fields);
+	//$product = new stdClass();
+	//$product = (object) $fields;
+	try {
+		$products = Bigcommerce::createProduct($fields);
+		print_r($products);
+		echo 'try';
+	} catch(Bigcommerce\Api\Error $error) {
+		echo $error -> getCode();
+		echo $error -> getMessage();
+	}
+}
+echo '</pre>';
 ?>
