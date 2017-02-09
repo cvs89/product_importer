7004cba7e1eaf8eccd881e7eaec894bdd9e614af
chandraveer <chandraveersingh.90@gmail.com>
2017-02-09 16:37:51 +0530

    add listing products

--- imported.php
+++ imported.php
@@ -19,9 +19,7 @@
 		}
 		if (isset($_GET['pid'])) {
 			$pid = $_GET['pid'];
-		} else {
-			exit("No store Available");
-		}
+		
 		$result_token = mysqli_query($link, "select * from stores where user_id=" . $user);
 		$user_rows = mysqli_fetch_assoc($result_token);
 		//echo $rows['access_token'];
@@ -89,6 +87,138 @@
 				echo $error -> getMessage();
 			}
 		}
+		} else {
+			$result = mysqli_query($link, "SELECT a.*, b.*, c.store_id as store_id FROM products as a join variants as b on (a.id = b.product_id) join store_products as c on (a.id=c.product_id and c.store_id='".$user_result['id']."')");
+
+//$categories = Bigcommerce::getCategories();
+//print_r($categories);
+//foreach ($categories as $categorie) {
+//echo $categorie -> id;
+//}
+?>
+<html>
+	<head>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	</head>
+	<body style="padding: 10px;">
+		<h2 style="text-align: center">Our Products</h2>
+		<div class="table-responsive">
+		<table class="table table-striped">
+	<thead>
+		<th>aTitle(Name)</th>
+		<th>Description</th>
+		<!--<th>URL-Key</th>-->
+		<th>Vendor</th>
+		<!--<th>Type</th>
+		<th>Tags</th>
+		 <th>Published</th> -->
+		<!--<th>Image</th>-->
+		<th>SEO Title</th>
+		<th>SEO Description</th>
+		<!--<th>Size</th>
+		<th>Color</th>
+		<th>SKU</th>
+		<th>Weight(grams)</th>-->
+		<th>Price</th>
+		<th>Compareat Price</th>
+		<!--<th>barcode</th>
+		<th>WeightUnit</th>
+		<th>Import</th>-->
+	</thead>
+	<tbody>
+		<?php
+while ($row = mysqli_fetch_assoc($result)) {
+	//print_r($row);
+		$image = array();
+		if($row['image']!= ''){
+			$image[] = $row['image'];
+		}
+		if($row['image1']!= ''){
+			$image[] = $row['image1'];
+		}
+		if($row['image2']!= ''){
+			$image[] = $row['image2'];
+		}
+		if($row['image3']!= ''){
+			$image[] = $row['image3'];
+		}
+		if($row['image4']!= ''){
+			$image[] = $row['image4'];
+		}
+		if($row['image5']!= ''){
+			$image[] = $row['image5'];
+		}
+		if($row['image6']!= ''){
+			$image[] = $row['image6'];
+		}
+		if($row['image7']!= ''){
+			$image[] = $row['image7'];
+		}
+		if($row['image8']!= ''){
+			$image[] = $row['image8'];
+		}
+		if($row['image9']!= ''){
+			$image[] = $row['image9'];
+		}
+		if($row['image10']!= ''){
+			$image[] = $row['image10'];
+		}
+		
+		?>
+		<tr>
+			<td><?php echo $row['title']; ?></td>
+			<td><?php echo substr($row['body'], 0, 50); ?></td>
+			<!--<td><?php echo $row['handle']; ?></td>-->
+			<td><?php echo $row['vendor']; ?></td>
+			 <!-- <td><?php echo $row['type']; ?></td>
+			<td><?php echo $row['tags']; ?></td>
+			<td><?php echo $row['published']; ?></td> -->
+			<!--<td><?php
+			foreach ($image as $img) {
+				if ($img != '') {
+					echo $img;
+				}
+
+			}
+				 ?></td>-->
+			<td><?php echo $row['seotitle']; ?></td>
+			<td><?php echo $row['seodescription']; ?></td>
+			<!--<td><?php echo $row['size']; ?></td>
+			<td><?php echo $row['color']; ?></td>
+			<td><?php echo $row['sku']; ?></td>
+			<td><?php echo $row['grams']; ?></td>-->
+			<td><?php echo $row['price']; ?></td>
+			<td><?php echo $row['compareatprice']; ?></td>
+			<!--<td><?php echo $row['barcode']; ?></td>
+			<td><?php echo $row['weightunit']; ?></td>
+			<td>
+			<?php
+			if(trim($row['store_id'])==""){
+			 ?>
+<a href="imported.php?user=<?php echo $user ?>&pid=<?php echo $row['product_id']; ?>">Import</a>
+<?php 
+			}else{
+				echo 'Imported';
+			}
+?>
+</td> -->
+		</tr>
+		<?php
+
+		}
+	?>
+	</tbody>
+</table>
+</div>
+			<?php
+			}
 	?>
 	</body>
 </html>
