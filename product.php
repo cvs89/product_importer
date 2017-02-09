804c044440c91895f441d34c42d228ec5d557f06
chandraveer <chandraveersingh.90@gmail.com>
2017-02-09 12:36:24 +0530

    add ui files

--- product.php
+++ product.php
@@ -52,7 +52,7 @@ $filter = array("is_featured" => true);
 	<tbody>
 		<?php
 while ($row = mysqli_fetch_assoc($result)) {
-	print_r($row);
+	//print_r($row);
 		/*$image = aaray();
 		if($row['image']!= ''){
 			$image[] = $row['image'];
@@ -91,7 +91,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 		?>
 		<tr>
 			<td><?php echo $row['title']; ?></td>
-			<?php /*<td><?php echo $row['body']; ?></td>
+			<td><?php echo $row['body']; ?></td>
 			<td><?php echo $row['handle']; ?></td>
 			<td><?php echo $row['vendor']; ?></td>
 			<td><?php echo $row['type']; ?></td>
@@ -103,7 +103,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 							echo $img;
 						}
 						
-					}
+					}*/
 				 ?></td>
 			<td><?php echo $row['seotitle']; ?></td>
 			<td><?php echo $row['seodescription']; ?></td>
@@ -115,7 +115,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 			<td><?php echo $row['compareatprice']; ?></td>
 			<td><?php echo $row['barcode']; ?></td>
 			<td><?php echo $row['weightunit']; ?></td>
-			<td><a href="imported.php?user=<?php echo $user ?>&pid=<?php echo $row['product_id']; ?>"</td> */ ?>
+			<td><a href="imported.php?user=<?php echo $user ?>&pid=<?php echo $row['product_id']; ?>"</td> 
 		</tr>
 		<?php
 
